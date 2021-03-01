a1f35946b8ebfa01a9f0e837c2639d69bfd7bf03
David Mulholland <davidmulholland@Davids-iMac.local>
2021-03-01 18:56:06 +0000

    text added for final projects and about me section completed

--- js/portfolio-indepth.js
+++ js/portfolio-indepth.js
@@ -115,7 +115,7 @@ const portfolio = [
   {
     name: 'Object oriented guessing game',
     description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem animi at vitae ullam incidunt? Hic sed quibusdam fugiat ratione. Officia et, quo non aliquam praesentium deserunt temporibus recusandae impedit. Optio.Nemo debitis pariatur qui dicta neque esse eaque distinctio, veritatis saepe doloremque animi, error assumenda, blanditiis repudiandae harum cupiditate cumque voluptatum ratione dolore molestias mollitia. Voluptatibus sapiente similique dolorum laboriosam.',
+      'This project taught me the fundamentals of object oriented programming. I created classes for game and phrase and created new instances when a new game is started. in addition I have added a background image which gives the player a clue about the phrases being from films, also I have added some animations with spins the correct letter once found using a bezier curve.',
     images: [
       './images/projects/OOP-won-screen.png',
       './images/projects/OOP-game-over-screen.png',
@@ -125,25 +125,14 @@ const portfolio = [
     keyFunctionality: [
       {
         Frontend: [
-          'Four product filters connected to redux to allow the user to filter products based on various criteria',
-          'JWT tokens used to connect to the back-end and authenticate users',
-          'A user can refresh the shopping cart screen and their product remains due to user information stored in local storage',
-          'Reviews stored on the product in the mongoDB database give the product a rating, this can be expanded by clicking on the review section of the product page and it will show all the reviews',
-          'If a user is signed in they can complete an order which will be sent to the database and they can see this on their account screen',
-          'They can write a review for a product they have purchased and it will be visible on the product and alter its overall rating. If they have reviewed the product already the database will not allow this and an error message will be displayed',
-          'The product page displays the amount of items in stock for that product including their size, and it stops the user from selecting products out of stock or more than available',
-          'The window object calculates when the navbar has scrolled out of view and fades in a up arrow which takes them to the top of the page if clicked. This arrow fades out if they scroll up and the navbar is in view',
-          'The user can make adjustments to their shopping basket including changing the amount or deleting an item. If they try and increase the amount beyond what is it stock they get an error',
-          'NPM package CSS-Transition group has been used to fade in the menus and also reviews',
-          'The user can use the search bar to search items in the site.',
+          "The user have 5 chances to guess the answer, if they can't they lose",
+          'Objects created for game, app and phrase which have methods which control functionality for the app',
+          'Each object accesses other objects to give or gather information, using the this keyword where appropriate',
+          'If the user guesses a word correctly then i have created an animation which spins the letter around to review it using CSS transform',
         ],
       },
       {
-        Backend: [
-          'Authenticate the user with JWT tokens',
-          'Hashes the user password using Bcrypt',
-          'Various routes such as User, Orders and Products which provide user data',
-        ],
+        Backend: ['N/A'],
       },
     ],
     gitHubLink: 'https://github.com/Webdev-DavidM/THFS-OOP-game-show',
@@ -152,7 +141,7 @@ const portfolio = [
   {
     name: 'Video player using the Youtube API',
     description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem animi at vitae ullam incidunt? Hic sed quibusdam fugiat ratione. Officia et, quo non aliquam praesentium deserunt temporibus recusandae impedit. Optio.Nemo debitis pariatur qui dicta neque esse eaque distinctio, veritatis saepe doloremque animi, error assumenda, blanditiis repudiandae harum cupiditate cumque voluptatum ratione dolore molestias mollitia. Voluptatibus sapiente similique dolorum laboriosam.',
+      'This project uses the youtube API along with React to show my favourite youtube videos. This was my first experience using React hooks. ',
     images: [
       './images/projects/project15-youtube.png',
       './images/projects/youtube-app-image2.png',
@@ -164,32 +153,21 @@ const portfolio = [
     keyFunctionality: [
       {
         Frontend: [
-          'Four product filters connected to redux to allow the user to filter products based on various criteria',
-          'JWT tokens used to connect to the back-end and authenticate users',
-          'A user can refresh the shopping cart screen and their product remains due to user information stored in local storage',
-          'Reviews stored on the product in the mongoDB database give the product a rating, this can be expanded by clicking on the review section of the product page and it will show all the reviews',
-          'If a user is signed in they can complete an order which will be sent to the database and they can see this on their account screen',
-          'They can write a review for a product they have purchased and it will be visible on the product and alter its overall rating. If they have reviewed the product already the database will not allow this and an error message will be displayed',
-          'The product page displays the amount of items in stock for that product including their size, and it stops the user from selecting products out of stock or more than available',
-          'The window object calculates when the navbar has scrolled out of view and fades in a up arrow which takes them to the top of the page if clicked. This arrow fades out if they scroll up and the navbar is in view',
-          'The user can make adjustments to their shopping basket including changing the amount or deleting an item. If they try and increase the amount beyond what is it stock they get an error',
-          'NPM package CSS-Transition group has been used to fade in the menus and also reviews',
-          'The user can use the search bar to search items in the site.',
+          'The application connects to my youtube API account to access and display my favourite youtube videos from one of my saved playlists',
+          'Once a video is selected I use a modal window to grey out the background.',
+          'The modal window can be closed by clicking outside of the modal window',
+          'I use react hooks to store the list of videos, the currently selected video and when to show the modal window',
         ],
       },
       {
-        Backend: [
-          'Authenticate the user with JWT tokens',
-          'Hashes the user password using Bcrypt',
-          'Various routes such as User, Orders and Products which provide user data',
-        ],
+        Backend: ['N/A'],
       },
     ],
   },
   {
     name: 'Web dashboard',
     description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem animi at vitae ullam incidunt? Hic sed quibusdam fugiat ratione. Officia et, quo non aliquam praesentium deserunt temporibus recusandae impedit. Optio.Nemo debitis pariatur qui dicta neque esse eaque distinctio, veritatis saepe doloremque animi, error assumenda, blanditiis repudiandae harum cupiditate cumque voluptatum ratione dolore molestias mollitia. Voluptatibus sapiente similique dolorum laboriosam.',
+      'This was one of my projects as part of the Treehouse front-end tech degree. I created an interactive web dashboard which includes alot of functionality detailed below. The UI is built with a combination of CSS grid for the main sections, flexbox to control layout within the sections, Jquery for some interactive features and SVG icons which can scale for layout at different sizes',
     images: [
       './images/projects/project4-dashboard-image1.png',
       './images/projects/project4-dashboard-image2.png',
@@ -203,25 +181,15 @@ const portfolio = [
     keyFunctionality: [
       {
         Frontend: [
-          'Four product filters connected to redux to allow the user to filter products based on various criteria',
-          'JWT tokens used to connect to the back-end and authenticate users',
-          'A user can refresh the shopping cart screen and their product remains due to user information stored in local storage',
-          'Reviews stored on the product in the mongoDB database give the product a rating, this can be expanded by clicking on the review section of the product page and it will show all the reviews',
-          'If a user is signed in they can complete an order which will be sent to the database and they can see this on their account screen',
-          'They can write a review for a product they have purchased and it will be visible on the product and alter its overall rating. If they have reviewed the product already the database will not allow this and an error message will be displayed',
-          'The product page displays the amount of items in stock for that product including their size, and it stops the user from selecting products out of stock or more than available',
-          'The window object calculates when the navbar has scrolled out of view and fades in a up arrow which takes them to the top of the page if clicked. This arrow fades out if they scroll up and the navbar is in view',
-          'The user can make adjustments to their shopping basket including changing the amount or deleting an item. If they try and increase the amount beyond what is it stock they get an error',
-          'NPM package CSS-Transition group has been used to fade in the menus and also reviews',
-          'The user can use the search bar to search items in the site.',
+          'The website uses scalable SVGs which adjust given the screen size',
+          ' I use an external library called Chart.JS, clicking the option above the graph animates the graph',
+          'Jquery is used to remove the alert',
+          ' You can search for a user to message at the bottom and a dynamic list appears',
+          'You can adjust settings at the bottom which are set in local storage, and persist if you refresh the page',
         ],
       },
       {
-        Backend: [
-          'Authenticate the user with JWT tokens',
-          'Hashes the user password using Bcrypt',
-          'Various routes such as User, Orders and Products which provide user data',
-        ],
+        Backend: ['N/A'],
       },
     ],
   },
